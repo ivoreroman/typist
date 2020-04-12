402eea3cc3ca1782a6c87f737d48802d41e98ac3
Martin Granados Garcia <margrangr@gmail.com>
2020-04-11 23:14:49 -0500

    Recording whenever someone speaks

--- pages/document.js
+++ pages/document.js
@@ -1,87 +1,76 @@
-import useSWR from "swr";
+import {useRef, useEffect, useState} from 'react';
+import useSWR from 'swr';
+import hark from 'hark';
+import getUserMedia from 'getusermedia';
 
-import { useRef, useEffect, useState } from "react";
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
-const fetcher = url => fetch(url).then(r => r.json());
-
-const Recorder = () => {
-  const [source, setSource] = useState(null);
+const DocumentPage = () => {
   const [src, setSrc] = useState(null);
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
   const audioCtx = useRef();
+  const source = useRef();
   const destination = useRef();
   const chunks = useRef();
-
-  const onRecordClick = () => {
-    if (mediaRecorder && destination.current && chunks.current && source) {
-      if (!isRecording) {
-        source.connect(destination.current);
-        mediaRecorder.start();
-        setIsRecording(true);
-      } else {
-        mediaRecorder.stop();
-        chunks.current = [];
-        setIsRecording(false);
-      }
-    }
-  };
 
   useEffect(() => {
     audioCtx.current = new (window.AudioContext || window.webkitAudioContext)();
     destination.current = audioCtx.current.createMediaStreamDestination();
     chunks.current = [];
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then(function(stream) {
-        setSource(audioCtx.current.createMediaStreamSource(stream));
-        const mediaRecorder = new MediaRecorder(destination.current.stream);
+    getUserMedia({video: false, audio: true}, (err, stream) => {
+      if (err) throw err;
 
-        mediaRecorder.ondataavailable = function(evt) {
-          chunks.current.push(evt.data);
-        };
-        mediaRecorder.onstop = function(evt) {
-          let blob = new Blob(chunks.current, {
-            type: "audio/flac; codecs=flac"
-          });
-          setSrc(URL.createObjectURL(blob));
-        };
+      source.current = audioCtx.current.createMediaStreamSource(stream);
+      const mediaRecorder = new MediaRecorder(destination.current.stream);
 
-        setMediaRecorder(mediaRecorder);
-      })
-      .catch(function(err) {
-        console.log("error getting audio => ", err);
+      mediaRecorder.ondataavailable = function(evt) {
+        chunks.current.push(evt.data);
+      };
+      mediaRecorder.onstop = function(evt) {
+        let blob = new Blob(chunks.current, {
+          type: 'audio/flac',
+        });
+        setSrc(URL.createObjectURL(blob));
+      };
+
+      setMediaRecorder(mediaRecorder);
+      let speechEvents = hark(stream, {});
+
+      speechEvents.on('speaking', () => {
+        console.log('You are speaking');
+        source.current.connect(destination.current);
+        mediaRecorder.start();
+        setIsRecording(true);
       });
+
+      speechEvents.on('stopped_speaking', () => {
+        console.log('You stopped speaking');
+        mediaRecorder.stop();
+        chunks.current = [];
+        setIsRecording(false);
+      });
+    });
   }, []);
 
-  return (
-    <>
-      <button onClick={onRecordClick}>{isRecording ? "Stop" : "Record"}</button>
-      {src && !isRecording && (
-        <audio controls>
-          <source src={src} type="audio/flac" />
-        </audio>
-      )}
-    </>
-  );
-};
-
-const DocumentPage = () => {
-  const { data: fetchedDoc, error } = useSWR("/api/documents");
+  const {data: fetchedDoc, error} = useSWR('/api/documents');
 
   if (error) return <div>failed to load</div>;
   if (!fetchedDoc) return <div>loading...</div>;
-
   return (
     <>
       <div>
-        <Recorder />
         <h1>Transcript for meeting {fetchedDoc.id}</h1>
         <em className="pulled-right">{fetchedDoc.createdAt}</em>
       </div>
       <div>
         <code>{fetchedDoc.doc}</code>
       </div>
+      {src && !isRecording && (
+        <audio controls>
+          <source src={src} type="audio/flac" />
+        </audio>
+      )}
     </>
   );
 };
